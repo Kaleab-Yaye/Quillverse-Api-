c40d9239cd0c27426b1cc828e268faebddeb021d
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-06-30 12:28:30 +0300

    the second commite

---
+++ src/main/java/com/quillverse/API/HelloConroler.java
@@ -0,0 +1,12 @@
+package com.quillverse.API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloConroler {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello back to you";
+    }
+}
