e8dbfc221ec32eb5beef9b081afff0c16ab17630
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-07-01 08:44:00 +0300

    the third commit

--- src/main/java/com/quillverse/API/HelloConroler.java
+++ src/main/java/com/quillverse/API/HelloConroler.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloConroler {
-    @GetMapping("/hello")
+    @GetMapping("//hello")
     public String hello(){
         return "hello back to you";
     }
 }
+
+
